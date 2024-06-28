5404202a2e17d0261c6b8e7ab3ce3c7b89cded11
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-28 19:14:35 +0200

    [Add] class Post

---
+++ src/domain/model/Post.java
@@ -0,0 +1,42 @@
+package domain.model;
+
+import java.util.ArrayList;
+
+public class Post {
+	private Integer id;
+	private Integer animalId;
+	private ArrayList<Integer> commentsIds;
+	public Post() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Post(Integer id, Integer animalId, ArrayList<Integer> commentsIds) {
+		super();
+		this.id = id;
+		this.animalId = animalId;
+		this.commentsIds = commentsIds;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getAnimalId() {
+		return animalId;
+	}
+	public void setAnimalId(Integer animalId) {
+		this.animalId = animalId;
+	}
+	public ArrayList<Integer> getCommentsIds() {
+		return commentsIds;
+	}
+	public void setCommentsIds(ArrayList<Integer> commentsIds) {
+		this.commentsIds = commentsIds;
+	}
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", animalId=" + animalId + ", commentsIds=" + commentsIds + "]";
+	}
+
+}
