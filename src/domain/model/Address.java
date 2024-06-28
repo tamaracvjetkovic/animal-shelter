2abb738598440a46780901b510cf1c35987b436a
DusicaPesic <130140285+DusicaPesic@users.noreply.github.com>
2024-06-28 18:54:08 +0200

    [Add] class Address

---
+++ src/domain/model/Address.java
@@ -0,0 +1,61 @@
+package domain.model;
+
+public class Address {
+	private int id;
+	private String city;
+	private String street;
+	private int number;
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address(int id, String city, String street, int number) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.street = street;
+		this.number = number;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", city=" + city + ", street=" + street + ", number=" + number + "]";
+	}
+
+	
+}
