0466af3bb7a5667b53927f5a0fa3402384280f39
Tamara Cvjetkovic <c.tamara02@gmail.com>
2024-06-30 03:02:33 +0200

    [REFACTOR] MainWindow window data

---
+++ src/util/UIUtils.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class UIUtils {
+
+    private UIUtils() {}
+
+    // centers the component in the centre of the desktop
+    public static void center(Component component) {
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int w = component.getSize().width;
+        int h = component.getSize().height;
+        int x = (dim.width - w) / 2;
+        int y = (dim.height - h) / 2;
+
+        // sets new location for the component
+        component.setLocation(x, y);
+    }
+}
