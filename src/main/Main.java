6afc49671695ddb372d6330e53458e2ae61482cf
Rogan003 <veselin.roganovic.rogan003@gmail.com>
2024-06-30 02:42:45 +0200

    [FEAT] Save happens at the end of the program

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,9 @@ import view.LoginWindow;
 import view.MainWindow;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -19,13 +22,14 @@ public class Main {
         try {
             Serialization serialization = new Serialization();
             serialization.load();
-            System.out.println(AnimalList.getInstance().getAnimals());
             MainWindow mainWindow = new MainWindow();
-//            PostList.getInstance().addPost(new Post(PostList.getInstance().generateId(), 1,
-//                    new ArrayList<>(Arrays.asList(1,2,3))));
-//            AnimalList.getInstance().addAnimal(new Animal(1, "Jezic", "braon", new Date(), 0,
-//                    AnimalState.NOTADOPTED, new ArrayList<>(), 0, 1));
-//            serialization.save();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    serialization.save();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }));
         }
         catch (Exception e) {
             System.out.println(e);
